81489fe2f9557dee5ac4bc27711b0b6907eadbb0
Koykoy200078 <franc200078@gmail.com>
2025-02-18 11:41:32 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Students;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(1000)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Students::create([
+            'name' => 'Male 1',
+            'age' => 25,
+            'gender' => 'male'
+        ]);
+        Students::create([
+            'name' => 'Female 1',
+            'age' => 27,
+            'gender' => 'female'
+        ]);
+        Students::create([
+            'name' => 'Female 2',
+            'age' => 23,
+            'gender' => 'female'
         ]);
     }
 }
